adf6912137c031eba39e78dbddc3dc19d533fab7
mrkyamamotoopatech-source <mr.k.yamamoto.opatech@gmail.com>
2026-01-22 12:59:22 +0900

    Fix SwiftPM linkage for scoring core

--- ios/VideoPickerScoring/Package.swift
+++ ios/VideoPickerScoring/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "VideoPickerScoring",
-            targets: ["VideoPickerScoring"]
+            targets: ["VideoPickerScoring", "VideoPickerScoringCore"]
         )
     ],
     targets: [
