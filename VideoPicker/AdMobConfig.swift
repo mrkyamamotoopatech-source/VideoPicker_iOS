5e493477fd54485091a9355435ddc8301d9b1230
Yamamoto-Keishi <yamamoto_keishi@lszzg.com>
2026-03-16 04:20:41 +0900

    広告IDを修正

--- VideoPicker/AdMobConfig.swift
+++ VideoPicker/AdMobConfig.swift
@@ -11,21 +11,43 @@ struct AdMobConfig {
     // App ID
     static let appID = "ca-app-pub-5859864934932113~9414286435"
     
+    // アプリがテストモードかどうかを判定
+    private static var isTestMode: Bool {
+        // App Store Connect、TestFlightからのインストールの場合はfalse
+        guard let appStoreReceiptURL = Bundle.main.appStoreReceiptURL else { return true }
+        let receiptURLString = appStoreReceiptURL.path
+        
+        // App Store、TestFlightの場合
+        if receiptURLString.contains("/Applications/") || receiptURLString.contains("CoreSimulator") {
+            return true // シミュレータやXcodeからの直接インストール
+        }
+        
+        if receiptURLString.contains("StoreKit/sandboxReceipt") {
+            return false // TestFlight
+        }
+        
+        if receiptURLString.contains("/StoreKit/receipt") {
+            return false // App Store
+        }
+        
+        return true // その他（開発時など）
+    }
+    
     // バナー広告ユニットID
     static var bannerAdUnitID: String {
-        #if DEBUG
-        return "ca-app-pub-3940256099942544/2934735716" // テスト用ID
-        #else
-        return "ca-app-pub-5859864934932113/4765758706" // 本番用
-        #endif
+        if isTestMode {
+            return "ca-app-pub-3940256099942544/2934735716" // テスト用ID
+        } else {
+            return "ca-app-pub-5859864934932113/4765758706" // 本番用
+        }
     }
     
     // インタースティシャル広告ユニットID
     static var interstitialAdUnitID: String {
-        #if DEBUG
-        return "ca-app-pub-3940256099942544/4411468910" // テスト用ID
-        #else
-        return "ca-app-pub-5859864934932113/5604293117" // 本番用
-        #endif
+        if isTestMode {
+            return "ca-app-pub-3940256099942544/4411468910" // テスト用ID
+        } else {
+            return "ca-app-pub-5859864934932113/5604293117" // 本番用
+        }
     }
 }
